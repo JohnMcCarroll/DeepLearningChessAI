f3e40877b8b8ab49ce6a07d8318b33b7fb4cd2da
jtm5356 <jtm5356@rit.edu>
2019-10-01 11:14:52 -0400

    implemented training loop and have begun experimenting with CNN architectures

--- CNN.py
+++ CNN.py
@@ -4,7 +4,9 @@ import torch.nn.functional as F
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(14, 6, 3, 1, 1)
+        self.conv1 = nn.Conv2d(14, 14, 3, 1, 1)
+        self.conv2 = nn.Conv2d(14, 14, 3, 1, 1)
+        self.conv3 = nn.Conv2d(14, 6, 3, 1, 1)
         self.out = nn.Linear(8*8*6, 1)
 
     def forward(self, t):
@@ -14,6 +16,14 @@ class CNN(nn.Module):
         # conv1 layer
         t = self.conv1(t)
         t = F.relu(t)
+
+        # conv2 layer
+        t = self.conv2(t)
+        t = F.relu(t)
+
+        # conv3 layer
+        t = self.conv3(t)
+        t = F.relu(t)        
 
         # linear output layer
         t = t.reshape(-1, 8*8*6)
--- TrainingLoop.py
+++ TrainingLoop.py
@@ -12,53 +12,61 @@ import matplotlib.pyplot as plt
 # loading in 
 
 # Creates new network
-#with open('D:\Machine Learning\DeepLearningChessAI\CNN_proto1.cnn', 'wb') as file:
+#with open('D:\Machine Learning\DeepLearningChessAI\CNN_yankee1.cnn', 'wb') as file:
 #    pickle.dump(CNN.CNN(), file)
 
 with open('D:\Machine Learning\DeepLearningChessAI\DatasetTest.db', 'rb') as file:
     train_set = pickle.load(file)
 
-with open('D:\Machine Learning\DeepLearningChessAI\CNN_proto1.cnn', 'rb') as file:
+with open('D:\Machine Learning\DeepLearningChessAI\CNN_yankee1.cnn', 'rb') as file:
     network = pickle.load(file)
 
 # initialize hyperparameters
 batchSize = 100
-learningRate = 0.0003
+learningRate = 0.001
+epoch = 2
 
 # setting up training data
-train_set, validation_set, dummy_set = torch.utils.data.random_split(train_set, [166000, 18000, 72])
+train_set, validation_set, dummy_set = torch.utils.data.random_split(train_set, [166000, 18000, 72])        #prob shouldnt be random split in val is benchmark between networks
 train_loader = torch.utils.data.DataLoader(train_set, batchSize, shuffle=True)
 optimizer = optim.Adam(network.parameters(), learningRate)
 
 # setting up validation data
-validation_loader = torch.utils.data.DataLoader(validation_set, 1000)
+validation_loader = torch.utils.data.DataLoader(validation_set, 1000)       # also shouldnt be random...
 val_boards, val_results = next(iter(validation_loader))
 val_results = val_results.float().reshape([-1, 1])
 val_losses = list()
 
-for batch in train_loader:
-    boards, results = batch
+for epoch in range(epoch):
+    for batch in train_loader:
+        boards, results = batch
 
-    # converting type & reshaping
-    results = results.float().reshape([-1, 1])
+        # converting type & reshaping
+        results = results.float().reshape([-1, 1])
 
-    # calculating loss
-    preds = network(boards)
-    loss = F.mse_loss(preds, results)
+        # calculating loss
+        preds = network(boards)
+        loss = F.mse_loss(preds, results)
 
-    before = loss.item()
+        # calculating gradients
+        optimizer.zero_grad()   #clear out accumulated gradients
+        loss.backward()
+        optimizer.step() # updating weights
 
-    # calculating gradients
-    optimizer.zero_grad()   #clear out accumulated gradients
-    loss.backward()
-    optimizer.step() # updating weights
+        # benchmark if learning
+        preds = network(val_boards)
+        loss = F.mse_loss(preds, val_results)
+        val_losses.append(loss.item())
 
-    # benchmark if learning
-    preds = network(val_boards)
-    loss = F.mse_loss(preds, val_results)
-    val_losses.append(loss.item())
+    plt.plot(val_losses)
+    plt.ylabel('validation loss')
+    plt.xlabel('batch number')
+    plt.show()
 
-plt.plot(val_losses)
-plt.ylabel('validation loss')
-plt.xlabel('batch number')
-plt.show()
+# save network
+with open('D:\Machine Learning\DeepLearningChessAI\CNN_yankee1.cnn', 'wb') as file:
+    pickle.dump(network, file)
+
+
+
+# do i have protection against exploding/vanishing gradients/weights?
